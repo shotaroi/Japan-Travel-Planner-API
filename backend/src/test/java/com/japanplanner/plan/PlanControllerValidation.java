1bc1996bc015c6a4f8aeea4c092bd4f8091dad2d
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-06-10 03:24:18 +0200

    refactor(test): update mock annotation in PlanControllerValidation to use MockitoBean

--- backend/src/test/java/com/japanplanner/plan/PlanControllerValidation.java
+++ backend/src/test/java/com/japanplanner/plan/PlanControllerValidation.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,7 +23,7 @@ class PlanControllerValidation {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private TripPlanRepository tripPlanRepository;
 
     @Test
